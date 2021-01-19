a09739714d76c9036e2a488c71c7bae652f8d9b0
Henrik Edsparr <henrik.edsparr@gmail.com>
2021-01-19 21:33:08 +0100

    Day2 solved.

--- src/main/kotlin/Advent2.kt
+++ src/main/kotlin/Advent2.kt
@@ -1,10 +1,20 @@
-class advent2(data: List<String>){
+class Advent2(data: List<String>){
     private val input: List<Triple<Int, Int, Int>> = parseList(data)
+
+    fun solvePart1(): Int {
+        val dimensions = calculateDimensions(input)
+        return dimensions.sum()
+    }
+
+    fun solvePart2(): Int {
+        val lengths = calculateLengths(input)
+        return lengths.sum()
+    }
 
     fun parseList(dataList: List<String>): List<Triple<Int, Int, Int>> {
         val listOfDimensions =  mutableListOf<Triple<Int, Int, Int>>()
             for (data in dataList){
-                val dimensions = data.split("#")
+                val dimensions = data.split("x")
                 listOfDimensions.add(Triple(dimensions[0].toInt(), dimensions[1].toInt(),dimensions[2].toInt()))
             }
         return listOfDimensions
@@ -25,6 +35,22 @@ class advent2(data: List<String>){
         val sideThree = dimension.second*dimension.third
         val minSide = minOf(sideOne, sideTwo, sideThree)
         return ((sideOne*2)+(sideTwo*2)+(sideThree*2)+minSide)
+    }
+
+    fun calculateLengths(listOfDimensions: List<Triple<Int, Int, Int>>): List<Int>{
+        val listOfLengths: MutableList<Int> = mutableListOf<Int>()
+        for (dimension in listOfDimensions){
+            val length = calculateLength(dimension)
+            listOfLengths.add(length)
+        }
+        return listOfLengths
+    }
+
+    fun calculateLength(dimension: Triple<Int, Int, Int>): Int {
+        val sideOne = minOf(dimension.first, dimension.second, dimension.third)
+        val sideTwo = maxOf(minOf(dimension.first, dimension.second), minOf(maxOf(dimension.first, dimension.second), dimension.third))
+        val volume = dimension.first*dimension.second*dimension.third
+        return ((sideOne*2)+(sideTwo*2)+volume)
     }
 
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,5 +7,12 @@ fun main() {
     val p2 = day1.solvePart2()
     **/
 
+    /** Advent day 2
     val input = resourceAsList("src/main/resources/advent2.txt")
+    val day2 = Advent2(input)
+    val p1 = day2.solvePart1()
+    val p2 = day2.solvePart2()
+    println(p1)
+    println(p2)
+    **/
 }
